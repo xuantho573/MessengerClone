5c4fc2427e561f755c0b7664d1284247f3071a2f
unknown <xuantho573@gmail.com>
2022-07-19 14:14:02 +0700

    Create model for conversations

--- backend/models/Conversations.model.js
+++ backend/models/Conversations.model.js
@@ -1,14 +1,18 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
+const ObjectId = mongoose.Types.ObjectId;
 const Schema = mongoose.Schema;
-const ConversationSchema = new Schema({
-    name: { type: String, required: true },
-    avatar: { type: String, required: true },
-    members: { type: Array, required: true },
-    messages: { type: Array, required: true },
-    mainEmote: { type: String, required: true }
-}, {
-    timestamps: true
-});
+const ConversationSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        avatar: { type: String, required: true },
+        members: { type: [ObjectId], required: true },
+        messages: { type: [ObjectId], required: true },
+        mainEmote: { type: String, required: true },
+    },
+    {
+        timestamps: true,
+    }
+);
 
-export default mongoose.model('Conversation', ConversationSchema);
+export default mongoose.model("Conversation", ConversationSchema);
