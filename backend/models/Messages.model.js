8eb045d837308b1a0e84871b6345496059383928
unknown <xuantho573@gmail.com>
2022-07-19 14:14:22 +0700

    Create model for messages

--- backend/models/Messages.model.js
+++ backend/models/Messages.model.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
 
 const ObjectId = mongoose.Types.ObjectId;
-
 const Schema = mongoose.Schema;
 const MessageSchema = new Schema(
     {
         author: { type: ObjectId, required: true },
         content: { type: String, required: true },
-        hiddenForUsers: { type: Array, required: true },
+        hiddenForUsers: { type: [ObjectId], required: true },
     },
     {
         timestamps: true,
