fe8606aa47da1397d7da3c3400f4b60902e4bdcb
unknown <xuantho573@gmail.com>
2022-07-19 14:14:45 +0700

    Create model for users

--- backend/models/Users.model.js
+++ backend/models/Users.model.js
@@ -1,10 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
+const ObjectId = mongoose.Types.ObjectId;
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
     name: { type: String, required: true },
-    conversations: { type: Array, required: true },
-    avatar: { type: String, required: true }
+    conversations: { type: [ObjectId], required: true },
+    avatar: { type: String, required: true },
 });
 
-module.exports = mongoose.model('UserSchema', UserSchema);
+export default mongoose.model("User", UserSchema);
