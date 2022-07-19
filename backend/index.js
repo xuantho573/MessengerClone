2da3f55b601902a25182e40592cf4b460ce85783
unknown <xuantho573@gmail.com>
2022-07-19 14:19:24 +0700

    Big commit on 19-07-2022

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import * as dotenv from "dotenv";
+
+import userRouter from "./routes/user.routes.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.static("/resources"));
+// app.use(morgan('combined'));
+app.use(userRouter);
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
