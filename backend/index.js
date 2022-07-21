c28ecb16374150787619579abb1e883fb46b22a8
unknown <xuantho573@gmail.com>
2022-07-21 11:45:52 +0700

    Add messageRouter to app

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import * as dotenv from "dotenv";
 
 import userRouter from "./routes/user.routes.js";
+import messageRouter from "./routes/message.routes.js";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const port = process.env.PORT || 8000;
 
 app.use(express.static("/resources"));
 app.use(userRouter);
+app.use(messageRouter);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
