a8f40a5aa125ba1a9be362a2fc292528b0223aa6
unknown <xuantho573@gmail.com>
2022-07-30 15:59:29 +0700

    Add 'salt' field and replace 'password' with 'hashedPassword' field

--- backend/models/Users.model.js
+++ backend/models/Users.model.js
@@ -4,7 +4,8 @@ const ObjectId = mongoose.Types.ObjectId;
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     username: { type: String, required: true },
-    password: { type: String, required: true },
+    hashedPassword: { type: String, required: true },
+    salt: { type: String, required: true },
     name: { type: String, required: true },
     conversations: { type: [ObjectId], required: true },
     avatar: { type: String, required: true },
