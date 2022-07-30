2c15cfef9881ac5338d5ecf7c91722cd485cde84
unknown <xuantho573@gmail.com>
2022-07-30 16:01:09 +0700

    Add mongoose object to 'connectDatabase()' and some loggings

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,10 @@
-import mongoose from "mongoose";
-
-const configDatabase = async (databaseEntry) => {
+const connectDatabase = async (mongoose, databaseEntry) => {
     try {
         await mongoose.connect(databaseEntry);
-        console.log(`Database connected successfully!`);
+        console.log(`Database connected successfully!\n`);
     } catch (err) {
-        console.log(`Errors detected while connecting to Mongoose`);
+        console.log(`Errors detected while connecting to Mongoose!\n`);
     }
 };
 
-export default configDatabase;
+export { connectDatabase };
