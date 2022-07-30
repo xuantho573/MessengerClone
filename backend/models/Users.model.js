62c57edd33adb253660b53f82619ee87b534eb8e
unknown <xuantho573@gmail.com>
2022-07-30 17:47:09 +0700

    Remove versionKey

--- backend/models/Users.model.js
+++ backend/models/Users.model.js
@@ -2,13 +2,16 @@ import mongoose from "mongoose";
 
 const ObjectId = mongoose.Types.ObjectId;
 const Schema = mongoose.Schema;
-const UserSchema = new Schema({
-    username: { type: String, required: true },
-    hashedPassword: { type: String, required: true },
-    salt: { type: String, required: true },
-    name: { type: String, required: true },
-    conversations: { type: [ObjectId], required: true },
-    avatar: { type: String, required: true },
-});
+const UserSchema = new Schema(
+    {
+        username: { type: String, required: true },
+        hashedPassword: { type: String, required: true },
+        salt: { type: String, required: true },
+        name: { type: String, required: true },
+        conversations: { type: [ObjectId], required: true },
+        avatar: { type: String, required: true },
+    },
+    { versionKey: false }
+);
 
 export default mongoose.model("User", UserSchema);
